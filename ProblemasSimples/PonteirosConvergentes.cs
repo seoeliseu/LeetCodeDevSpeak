2ff7c21d320cb93a1a7929637ba391996a390cf1
Eliseu Mendes de Morais <seoeliseu@gmail.com>
2025-04-03 21:24:16 -0300

    removido usings

--- ProblemasSimples/PonteirosConvergentes.cs
+++ ProblemasSimples/PonteirosConvergentes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProblemasSimples
+﻿namespace ProblemasSimples
 {
     public class PonteirosConvergentes
     {
